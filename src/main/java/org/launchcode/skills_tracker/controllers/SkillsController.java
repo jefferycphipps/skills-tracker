377e6a4645a7059758ca9f7e5cdbc07873b4b725
Jeffery Phipps <jefferyphipps@att.net>
2024-09-30 19:10:27 -0500

    Creating Git

--- src/main/java/org/launchcode/skills_tracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skills_tracker/controllers/SkillsController.java
@@ -1,4 +1,32 @@
 package org.launchcode.skills_tracker.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SkillsController {
+
+    @GetMapping()
+    public String hello(){
+        String html = "<html>" +
+                "<body>" +
+                "<form method = 'get' action = '/hello'>" +
+                "<input type = 'text' name = 'name' />" +
+                "<select name = 'language' id = 'language-select>"+
+                "<option value=''>--Please choose an option--</option>"+
+                "<option value='english'>English</option>"+
+                "<option value='french'>French</option>" +
+                "<option value='italian'>Italian</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='german'>German</option>"+
+                "</select>" +
+                "<input type = 'submit' value = 'Greet Me!' />" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+
+        return html;
+    }
+
 }
